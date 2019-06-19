cd8818abfd7ff92de2fcfb7c6e063bf18331af51
Peyman Noursalehi <peymano@transitlab2.mit.edu>
2019-06-19 17:18:33 -0400

    changed  visualize_know_f_2000.py

--- visualize_know_f_2000.py
+++ visualize_know_f_2000.py
@@ -10,8 +10,8 @@ def get_operation_cost(pro_share, fleet):
     "hourly cost of Via driver: $30"
     return (fleet * 30)
 #template = "./Outputs/RL/report for fleet size 2000 surge 2fdemand= 0.0perc_k 0pro_s 0 perc_av {} repl{}.csv"
-#template = "./Outputs/report for fleet size 1500 surge 2fdemand= 0.0perc_k {}pro_s 0 repl{}.csv"
-template = "./Outputs/report for fleet size 2000 surge 2fdemand= 0.0perc_k {}pro_s 0 repl{}.csv"
+template = "./Outputs/report for fleet size 1500 surge 2fdemand= 0.0perc_k {}pro_s 0 repl{}.csv"
+#template = "./Outputs/report for fleet size 2000 surge 2fdemand= 0.0perc_k {}pro_s 0 repl{}.csv"
 op_rev = []
 op_cost = []
 los_list = []
@@ -20,8 +20,9 @@ los_median = []
 denied_w = []
 ff = []
 fleet = 1500
+n_repl = 10
 for av_share in [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]:
-    for repl in range(5):
+    for repl in range(n_repl):
         report = pd.read_csv(template.format(av_share, repl))
         # op_rev.append(np.sum(m.operator.revenues))
         # op_cost.append(get_operation_cost(fleet,pro_share ))
@@ -42,7 +43,7 @@ los_list = np.array(los_list)
 data = pd.DataFrame.from_records([los_list, los_mean])
 df = data.transpose()
 df.columns = columns=["LOS", "mean"]
-df.index = np.repeat([0.0, 0.2,0.4, 0.6, 0.8, 1.0],5)
+df.index = np.repeat([0.0, 0.2,0.4, 0.6, 0.8, 1.0],n_repl)
 df["Ratio"] = df.index
 sns_plot = sns.boxplot(x="Ratio",y="LOS",data=df, palette="tab10", linewidth=2.5)
 plt.savefig("{}/los.png".format(directory))
